294437642aab2d352b6a5e7c966b4e15aa133717
Akshay Sathiya <akshay.sathiya@gmail.com>
2018-11-24 00:10:14 -0500

    8th commit

--- Riddle Me This/Riddle Me This/LevelTwo.swift
+++ Riddle Me This/Riddle Me This/LevelTwo.swift
@@ -11,14 +11,45 @@ import UIKit
 class LevelTwo: UIViewController {
     
     var playerScore = Int()
+    
+    var correctButton : Bool = false
 
     @IBOutlet weak var scoreLabel: UILabel!
+    
+    
+    @IBAction func cloudPressed(_ sender: Any) {
+        performSegue(withIdentifier: "wrongAnswerLvl2Segue", sender: self)
+    }
+    
+    
+    @IBAction func powerPressed(_ sender: Any) {
+        performSegue(withIdentifier: "wrongAnswerLvl2Segue", sender: self)
+    }
+    
+    @IBAction func compassPressed(_ sender: Any) {
+        performSegue(withIdentifier: "wrongAnswerLvl2Segue", sender: self)
+    }
+    
+    
+    @IBAction func heartPressed(_ sender: Any) {
+        correctButton = true
+        performSegue(withIdentifier: "correctLvl2Segue", sender: self)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         scoreLabel.text = "Score: \(playerScore)"
     
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (correctButton) {
+            
+        } else {
+            var gameOver = segue.destination as! GameOver
+            gameOver.finalScore = playerScore
+        }
     }
 
 }
