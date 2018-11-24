970ab5141e411a64390e952890277ec4bf1bea02
Akshay Sathiya <akshay.sathiya@gmail.com>
2018-11-23 23:36:21 -0500

    7th commit

--- Riddle Me This/Riddle Me This/LevelTwo.swift
+++ Riddle Me This/Riddle Me This/LevelTwo.swift
@@ -12,21 +12,13 @@ class LevelTwo: UIViewController {
     
     var playerScore = Int()
 
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+        scoreLabel.text = "Score: \(playerScore)"
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
 
 }
